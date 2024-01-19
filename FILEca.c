100696a330deb76fb56b168d4c19bef60e5c6c40
Sid P <155798013+TORTORPEDO@users.noreply.github.com>
2024-01-19 11:03:59 -0500

    Add files via upload

---
+++ FILEca.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (void) {
+    printf("Hello. Welcome and Please accept our terms and policy... \n ");
+
+
+    /* designate variables int*/
+    int battery = 12;
+    int lukewarm = 48;
+    int median;
+    int medallion;
+    
+    printf("The integer for a cup of lukewarm water is, %d \n" , lukewarm);
+
+    printf("The integer for our engine battery is, %d \n", battery);
+        
+             /* statement that designates multiplier*/
+            /* print integer for medallion output*/
+
+        medallion = battery * lukewarm;
+
+        printf("if my morning tea got onto the battery it would be!");
+         printf("overload of, %d", medallion);
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# C2A
-Introduction, function, variables
+# AlphabetSOUP
+# includes header library functions
+
+# declares variables as integer
+
+# prints variables on new line by \n declaration
